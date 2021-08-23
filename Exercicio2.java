351c3e493cc342d2283aa155f6b65840cbfa1d13
carlam97 <carmartins04@hotmail.com>
2021-08-23 01:46:51 +0000

    soma de 1 a 1000

---
+++ Exercicio2.java
@@ -0,0 +1,10 @@
+// soma de 1 a 1000
+public class Exercicio2{
+    public static void main(String args []){
+        int resultado = 0;
+        for(int x=1; x<=1000; x++){
+            resultado+=x;
+            System.out.println(resultado);
+        }
+    }
+}
